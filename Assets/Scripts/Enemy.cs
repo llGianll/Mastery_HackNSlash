5e1e26a6904d237337b7b9691271bf02ae80707e
Gian Carlo Sebastian <gsebastian.gamedev@gmail.com>
2021-12-20 17:27:20 +0800

    16-C Death and Animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour, ITakeHit
 {
+    Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
     public void TakeHit(Character hitBy)
     {
-        Destroy(gameObject);
+        _animator.SetTrigger("Die");
+        Destroy(gameObject, 6f);
     }
 }
