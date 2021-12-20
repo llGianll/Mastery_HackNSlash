82af6ebad63d5094539b8dc2ae35d5be40cf1205
Gian Carlo Sebastian <gsebastian.gamedev@gmail.com>
2021-12-20 18:01:50 +0800

    16-E Health

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Enemy : MonoBehaviour, ITakeHit
 {
     [SerializeField] GameObject _impactParticle;
+    [SerializeField] int _maxHealth = 3;
+
+    int _currentHealth;
 
     Animator _animator;
 
@@ -13,11 +16,26 @@ public class Enemy : MonoBehaviour, ITakeHit
         _animator = GetComponentInChildren<Animator>();
     }
 
+    private void OnEnable()
+    {
+        _currentHealth = _maxHealth;
+    }
+
     public void TakeHit(Character hitBy)
     {
-        _animator.SetTrigger("Die");
+        _currentHealth--;
 
         Instantiate(_impactParticle, transform.position + new Vector3(0, 2, 0), Quaternion.identity);
+
+        if (_currentHealth <= 0)
+            Die();
+        else
+            _animator.SetTrigger("Hit");
+    }
+
+    private void Die()
+    {
+        _animator.SetTrigger("Die");
 
         Destroy(gameObject, 6f);
     }
